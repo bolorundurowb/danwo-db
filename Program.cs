0a40ee580dd56b4d4ea3a7cd65037f29baa4c5a4
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2023-05-22 00:02:38 +0100

    ft: add in test logic to validate the work so far

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+
+var dal = new DataAccessLayer("database.db", (uint)Environment.SystemPageSize);
+var page = dal.AllocateEmptyPage();
+page.PageNumber = dal.FreeList.GetNextPage();
+
+var test = "my test data";
+var dataBuffer = Encoding.UTF8.GetBytes(test);
+dataBuffer.CopyTo(page.Data, 0);
+
+await dal.WritePage(page);
 
 Console.WriteLine("Hello, World!");
 
@@ -17,9 +27,12 @@ class DataAccessLayer : IAsyncDisposable
 
     public FileStream File { get; set; }
 
+    public FreeList FreeList { get; set; }
+
     public DataAccessLayer(string path, uint pageSize)
     {
         PageSize = pageSize;
+        FreeList = new FreeList();
         File = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read);
     }
 
@@ -40,11 +53,12 @@ class DataAccessLayer : IAsyncDisposable
         return page;
     }
 
-    public void WritePage(Page page)
+    public async Task WritePage(Page page)
     {
         var offset = page.PageNumber * PageSize;
-        var writer = new BinaryWriter(File);
-        writer.Seek((int)offset, SeekOrigin.Begin);
+        File.Seek(offset, SeekOrigin.Begin);
+        await File.WriteAsync(page.Data, 0, (int)PageSize);
+        File.Seek(0, SeekOrigin.Begin);
     }
 }
 
@@ -52,20 +66,22 @@ class FreeList
 {
     public uint MaxPage { get; set; }
 
-    public uint[] ReleasedPages { get; set; }
+    public List<uint> ReleasedPages { get; set; }
 
-    public FreeList() => ReleasedPages = Array.Empty<uint>();
+    public FreeList() => ReleasedPages = new List<uint>();
 
     public uint GetNextPage()
     {
         if (ReleasedPages.Any())
         {
             var pageNumber = ReleasedPages.Last();
-            ReleasedPages = ReleasedPages.Where(x => x != pageNumber).ToArray();
+            ReleasedPages = ReleasedPages.Where(x => x != pageNumber).ToList();
             return pageNumber;
         }
 
         MaxPage += 1;
         return MaxPage;
     }
+
+    public void ReleasePage(uint pageNumber) => ReleasedPages.Add(pageNumber);
 }
